d60720365025bee749ae976023ef53f343db952a
Zoran <tre.zor.ni@gmail.com>
2015-12-19 20:09:36 +0000

    Added contats controller

---
+++ app/controllers/contats_controller.rb
@@ -0,0 +1,7 @@
+class ContactsControler < ApplicationControler
+    def new
+    end
+    
+    def create
+    end
+end
--- db/migrate/20151218222629_create_contats.rb
+++ db/migrate/20151218222629_create_contats.rb
@@ -1,6 +1,11 @@
 class CreateContats < ActiveRecord::Migration
   def change
     create_table :contats do |t|
+      t.string :name
+      t.string :email
+      t.text :comments
+      
+      t.timestamps
     end
   end
 end
